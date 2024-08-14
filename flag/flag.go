9620bfab602d8be4e64381b758781a4e19154f85
togondol <obimbira60@gmail.com>
2024-08-14 13:49:13 +0300

    flag validation

--- flag/flag.go
+++ flag/flag.go
@@ -1,36 +1,28 @@
 package flags
 
 import (
-	"fmt"
-	"strings"
+    "fmt"
+    "strings"
 )
 
 func CheckFlag(args []string) string {
-	var count int
-	for _, arg := range args {
-		if strings.Contains(arg, "--") {
-			count++
-		}
-	}
-	var alignment string
+    if len(args) == 0 || !strings.HasPrefix(args[0], "--align=") {
+        Usage()
+    }
 
-	if strings.HasPrefix(args[0], "--align=") {
-		alignment = strings.ToLower(strings.TrimPrefix(args[0], "--align="))
-	}
+    parts := strings.SplitN(args[0], "=", 2)
+    if len(parts) != 2 || parts[0] != "--align" {
+        Usage()
+    }
 
-	if alignment != "right" &&
-		alignment != "justify" &&
-		alignment != "left" &&
-		alignment != "center" {
-		Usage()
-	}
-	if count > 1 {
-		Usage()
-	}
+    alignment := strings.TrimSpace(strings.ToLower(parts[1]))
+    if alignment != "right" && alignment != "justify" && alignment != "left" && alignment != "center" {
+        Usage()
+    }
 
-	return alignment
+    return alignment
 }
 
 func Usage() {
-	fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
+    fmt.Println("Usage: go run . [OPTION] [STRING] [BANNER]\n\nExample: go run . --align=right something standard")
 }
